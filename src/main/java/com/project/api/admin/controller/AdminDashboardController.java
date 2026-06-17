9984e2d0ab625dd103d6e95623234f176c2ca551
cjalonzaga <cjalonzaga@gmail.com>
2026-06-17 22:03:23 +0800

    file updates: still having issue on session login but - working on jwt auth

---
+++ src/main/java/com/project/api/admin/controller/AdminDashboardController.java
@@ -0,0 +1,16 @@
+package com.project.api.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminDashboardController {
+
+    @GetMapping("/dashboard")
+    public String home(Model model)  {
+        return "dashboard";
+    }
+}
