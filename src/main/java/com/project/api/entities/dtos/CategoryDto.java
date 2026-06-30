a1346bd51795b48b11af54ff30cb364559b4e72a
Cris John Alonzaga <cjalonzaga@gmail.com>
2026-06-30 09:08:58 +0800

    Category DTO and UI updates

--- src/main/java/com/project/api/entities/dtos/CategoryDto.java
+++ src/main/java/com/project/api/entities/dtos/CategoryDto.java
@@ -1,6 +1,7 @@
 package com.project.api.entities.dtos;
 
 import java.util.List;
+import java.util.Set;
 
 public class CategoryDto {
 	
@@ -12,4 +13,55 @@ public class CategoryDto {
 	
 	private CategoryDto parent;
 	
+	private List<CategoryDto> children;
+	
+	private Set<ProductCategoryDto> productCategorySet;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public CategoryDto getParent() {
+		return parent;
+	}
+
+	public void setParent(CategoryDto parent) {
+		this.parent = parent;
+	}
+
+	public List<CategoryDto> getChildren() {
+		return children;
+	}
+
+	public void setChildren(List<CategoryDto> children) {
+		this.children = children;
+	}
+
+	public Set<ProductCategoryDto> getProductCategorySet() {
+		return productCategorySet;
+	}
+
+	public void setProductCategorySet(Set<ProductCategoryDto> productCategorySet) {
+		this.productCategorySet = productCategorySet;
+	}
 }
